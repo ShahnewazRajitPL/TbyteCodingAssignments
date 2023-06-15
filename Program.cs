e4c4e8e79a44d5cdec6e1e7952188833c3631384
Shahnewaz Muhammad Rajit <135220468+ShahnewazRajitPL@users.noreply.github.com>
2023-06-16 01:08:13 +0600

    Displaying All the selected products which are selected by the user.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿namespace CodingAssignment
+namespace CodingAssignment
 {
     internal class Program
     {
@@ -14,6 +14,16 @@
             Product[] products = new Product[6];
             DisplayProducts(products);
 
+            List<SelectedProducts> selectedProducts = new List<SelectedProducts>();
+
+            // ***************** CHOOSING PRODUCTS ******************
+            buyProducts(selectedProducts, products);
+
+            // **************** DISPLAYING SELECTED PRODUCT WHICH USER WANT TO BUY ***************
+
+            displaySelectedProducts(selectedProducts);
+
+            Console.WriteLine();
 
 
 
@@ -37,6 +47,82 @@
             }
             Console.ResetColor();
         }
+
+
+        // ******************** BUY PRODUCTS *******************
+        public static void buyProducts(List<SelectedProducts> selectedProducts, Product[] products)
+        {
+            while (true)
+            {
+                Console.Write("Write the name of the product you want to buy (done to skip): ");
+                string select = Console.ReadLine();
+                if (select?.ToLower().Equals("done") == true)
+                {
+                    break;
+                }
+                int flag = 0;
+                for (int i = 0; i < products.Length; i++)
+                {
+                    if (select?.ToLower().Equals(products[i].productName.ToLower()) == true)
+                    {
+                        flag = 1;
+                        Console.Write("Enter the quantity: ");
+                        int quantity = Convert.ToInt32(Console.ReadLine());
+                        if (quantity > products[i].productQuantity)
+                        {
+                            Console.WriteLine("We don't have enough quantity of this product");
+                            Console.WriteLine();
+                        }
+                        else
+                        {
+                            Console.WriteLine();
+                            products[i].productQuantity = products[i].productQuantity - quantity;
+                            selectedProducts.Add(new SelectedProducts(products[i].productName, products[i].productPrice, quantity));
+                        }
+                    }
+                }
+
+                if (flag == 1)
+                {
+                    Console.Write("Do you want to add more? (yes/no): ");
+                    string decision = Console.ReadLine();
+                    if (decision?.Equals("yes") == true)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                else if (flag == 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Your product name is invalid, please correct it!!");
+                    Console.ResetColor();
+                    Console.WriteLine();
+                }
+            }
+        }
+
+        //********************** DISPLAYING SELECTED PRODUCTS *********************
+
+        public static void displaySelectedProducts(List<SelectedProducts> selectedProducts)
+        {
+            if (selectedProducts.Count >= 1)
+            {
+                Console.WriteLine();
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("The Selected products are: ");
+                Console.ResetColor();
+
+                for (int i = 0; i < selectedProducts.Count; i++)
+                {
+                    Console.WriteLine($"Product Name: {selectedProducts[i].productName} {new string(' ', 6)} Product Price: ${selectedProducts[i].productPrice} {new string(' ', 6)} Product Quantity: {selectedProducts[i].productQuantity}");
+                }
+            }
+        }
+
     }
 
     class Product
@@ -52,4 +138,27 @@
             this.productQuantity = productQuantity;
         }
     }
+
+
+    class SelectedProducts
+    {
+        public string productName;
+        public double productPrice;
+        public int productQuantity;
+
+        public SelectedProducts(string productName, double productPrice, int productQuantity)
+        {
+            this.productName = productName;
+            this.productPrice = productPrice;
+            this.productQuantity = productQuantity;
+        }
+    }
+
+
+
+
+
+
+
+
 }
