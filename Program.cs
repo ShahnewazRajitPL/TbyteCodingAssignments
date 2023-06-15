8af638fdf263fd18ecd8ca051c2d372b5aab7a25
Fuad Hasan <86760828+fuadhasan01@users.noreply.github.com>
2023-06-16 00:30:37 +0600

    Add files via upload
    Here I create a product class where  product information are given

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace CodingAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Welcome To Invoice Console App");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Debug Squid Team");
+
+            
+        }
+    }
+
+    class Product
+    {
+        public string productName;
+        public double productPrice;
+        public int productQuantity;
+
+        public Product(string productName, double productPrice, int productQuantity)
+        {
+            this.productName = productName;
+            this.productPrice = productPrice;
+            this.productQuantity = productQuantity;
+        }
+    }
+}
