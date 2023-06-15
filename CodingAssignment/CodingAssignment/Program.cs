5e479f5b01043596862ac785b0947d2d31eae353
MD. Shojib Talukder <109905033+AdilHasanShojib@users.noreply.github.com>
2023-06-15 22:28:49 +0600

    Coloring Welcome Message

---
+++ CodingAssignment/CodingAssignment/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace CodingAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Welcome To Invoice Console App");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Debug Squid Team");
+
+            Console.Clear();
+        }
+    }
+}
